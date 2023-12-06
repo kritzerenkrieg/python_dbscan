e5fff7498677c0f87444bc1a5e3bcdc7ad124e32
unknown <kentaro.keren@gmail.com>
2023-12-06 11:32:55 +0700

    added multiple cluster labels

--- dbscan.py
+++ dbscan.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import pairwise_distances_argmin_min
 
 # URL of the CSV file
 csv_url = "https://raw.githubusercontent.com/kritzerenkrieg/python_dbscan/main/data_sekolah_mojokerto.csv" 
@@ -18,6 +19,13 @@ df['Longitude'] = pd.to_numeric(df['Longitude'])
 # Adjust the column names accordingly
 X = df[['Latitude', 'Longitude']]
 
+# Calculating the centroid
+centroid = X.mean()
+
+# Displaying the centroid
+print("Centroid:")
+print(centroid)
+
 # Standardize the features
 X = StandardScaler().fit_transform(X)
 
@@ -28,11 +36,19 @@ labels = dbscan.fit_predict(X)
 # Add cluster labels to the DataFrame
 df['Cluster'] = labels
 
+# Calculate the centroid of each cluster
+centroids = df.groupby('Cluster')[['Latitude', 'Longitude']].mean()
+
+# Calculate each data to data centroid
+df['Distance_to_Centroid'] = ((df['Latitude'] - centroid['Latitude'])**2 + (df['Longitude'] - centroid['Longitude'])**2)**0.5
+
 # Displaying the clustered DataFrame
 print(df)
 
 # Visualize the clusters (for 2D data)
 plt.scatter(df['Latitude'], df['Longitude'], c=df['Cluster'], cmap='viridis')
+plt.scatter(centroids['Latitude'], centroids['Longitude'], marker='X', c='red', label='Cluster Centroids')
+plt.scatter(centroid['Latitude'], centroid['Longitude'], marker='*', c='blue', label='Data Centroid')
 plt.title('DBSCAN Clustering')
 plt.xlabel('Latitude')
 plt.ylabel('Longitude')
